6b43635a9f59df905cf8dbe993c2698725c12c72
Ale Pereira <naitwa-alexandre@gmail.com>
2024-10-01 10:48:27 -0300

    create info content list

--- src/Components/Body/Info/Info.tsx
+++ src/Components/Body/Info/Info.tsx
@@ -1,29 +1,35 @@
-import React from 'react'
 import ImageIcon from '../../assets/Imagicon.png'
+import './styles/styles.css'
+import { infoData } from '../../../data/info'
+import { MdVerified } from "react-icons/md";
+import { IoIosArrowForward } from "react-icons/io";
+
+
 
 function Info() {
     return (
         <div>
-            <div className='info-title'>
-                <img src={ImageIcon} alt="Profile picture" />
-                <button>PROFILE PICTURE</button>
+            <div className='info-header'>
+                <img className='picture' src={ImageIcon} alt="Profile" />
+                <button className='button'>PROFILE PICTURE</button>
             </div>
 
             <div className='info-content'>
-                <ul>
-                    <li>
-                        Profile
-                    </li>
-                    <li>
-                        Bio
-                    </li>
-                    <li>
-                        Gender
-                    </li>
-                    <li>
-                        Age
-                    </li>
-                </ul>
+                    {infoData.map((op,i) => {
+                        return (
+                            <ul>
+                                <li key={i}>
+                                    <span>{op.option}</span>
+                                    <div className='content-values'>
+                                        <span className='value'>{op.value}</span>
+                                        <span className='verified-symbol'>{op.option === 'Profile' && <MdVerified />}</span>
+                                        <span><IoIosArrowForward /></span>
+                                    </div>
+                                </li>
+                                <hr />
+                            </ul>
+                        )
+                    })}
             </div>
         </div>
     )
---
+++ src/data/info.js
@@ -0,0 +1,34 @@
+export const infoData = [
+    {
+        option: 'Profile',
+        value: 'Scarlet',
+    },
+    {
+        option: 'Bio',
+        value: 'Edit',
+    },
+    {
+        option: 'Gender',
+        value: 'Add',
+    },
+    {
+        option: 'Age',
+        value: 'Add',
+    },
+    {
+        option: 'Ethinicity',
+        value: 'Caucasian',
+    },
+    {
+        option: 'Height',
+        value: '5\'9" / 1,82m',
+    },
+    {
+        option: 'Weight',
+        value: '75kg / 165lb',
+    },
+    {
+        option: 'Language',
+        value: 'English, Spanish, Portuguese',
+    },
+]
